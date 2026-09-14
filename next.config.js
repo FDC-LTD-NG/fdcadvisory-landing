db9ed5eeef460f0d1fe73188fd6c3e706eaa5566
favor-choji <garosepamchoji@gmail.com>
2026-09-15 00:07:18 +0100

    add a toml file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // swagger-ui-react uses ssr:false via dynamic import in the client component,
+//   // so no additional webpack config is required.
+// };
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swagger-ui-react uses ssr:false via dynamic import in the client component,
-  // so no additional webpack config is required.
+  turbopack: {
+    root: __dirname,
+  },
 };
 module.exports = nextConfig;
